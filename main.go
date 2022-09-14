c737bed8fafa19a5b80a9fd06aec104bb5ec0f53
Michael Bohn <michael.bohn@freiheit.com>
2022-09-14 15:58:19 +0200

    grpc dependency detection

--- main.go
+++ main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"encoding/json"
-	"io/ioutil"
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"io/fs"
 	"os"
 	"path"
+	"path/filepath"
+	"strings"
 )
 
+type Dependency struct {
+	From string `json:"from"`
+	To   string `json:"to"`
+}
+
 type Arch struct {
-	Services []string `json:"services"`
+	Services []string     `json:"services"`
+	Grpc     []Dependency `json:"grpc"`
 }
 
 func main() {
@@ -18,17 +30,24 @@ func main() {
 		panic(err)
 	}
 
-	svcs, err := os.ReadDir(path.Join(monoRepoPath, "services/"))
+	modName, err := readModuleName(monoRepoPath)
+	if err != nil {
+		panic(err)
+	}
+
+	svcPath := path.Join(monoRepoPath, "services/")
+	svcs, err := findServices(svcPath)
+	if err != nil {
+		panic(err)
+	}
+	grpc, err := analyzeGrpc(svcPath, modName, svcs)
 	if err != nil {
 		panic(err)
 	}
 
 	arch := Arch{
-		Services: []string{},
-	}
-
-	for _, svc := range svcs {
-		arch.Services = append(arch.Services, svc.Name())
+		Services: svcs,
+		Grpc:     grpc,
 	}
 
 	bytes, err := json.Marshal(arch)
@@ -36,11 +55,109 @@ func main() {
 		panic(err)
 	}
 
-	err = ioutil.WriteFile("arch.json", bytes, os.ModePerm)
+	err = os.WriteFile("arch.json", bytes, os.ModePerm)
 	if err != nil {
 		panic(err)
 	}
-	// TODO Extract services from monorepo and write them to a json file
-	// TODO Visualise services in archv online tool!
-	// TODO Extract grpc relations between services (from import) and write them to a json file
+}
+
+func findServices(path string) ([]string, error) {
+	folders, err := os.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+	svcs := make([]string, 0, len(folders))
+	for _, svc := range folders {
+		svcs = append(svcs, svc.Name())
+	}
+	return svcs, nil
+}
+
+func analyzeGrpc(svcRootPath, modName string, svcs []string) ([]Dependency, error) {
+
+	grpcDependencies := make([]Dependency, 0)
+
+	for _, svc := range svcs {
+		svcPath := path.Join(svcRootPath, svc)
+		grpcImports := make([]string, 0)
+		err := filepath.WalkDir(svcPath, func(path string, fs fs.DirEntry, err error) error {
+			if !fs.IsDir() && strings.HasSuffix(fs.Name(), ".go") {
+				fileGrpcImports, err := checkGrpcImports(path, modName)
+				if err != nil {
+					return err
+				}
+
+				for _, grpcImport := range fileGrpcImports {
+					if grpcImport != svc && !contains(grpcImports, grpcImport) {
+						grpcImports = append(grpcImports, grpcImport)
+					}
+				}
+			}
+			return nil
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		for _, grpcImport := range grpcImports {
+			grpcDependencies = append(grpcDependencies, Dependency{From: svc, To: grpcImport})
+		}
+	}
+	return grpcDependencies, nil
+}
+
+func contains(strs []string, str string) bool {
+	for _, s := range strs {
+		if s == str {
+			return true
+		}
+	}
+	return false
+}
+
+// TODO adapt this to return a list of service-names (just strings) that this service talks to
+func checkGrpcImports(path, modName string) ([]string, error) {
+	fset := token.NewFileSet()
+	ast, err := parser.ParseFile(fset, path, nil, parser.ImportsOnly)
+	if err != nil {
+		return nil, err
+	}
+
+	imports := make([]string, 0)
+
+	for _, imp := range ast.Imports {
+		if importHasPrefix(imp, modName) && importHasSuffix(imp, "pkg/proto") { // only check module internal imports
+			split := strings.Split(imp.Path.Value, "/")
+			if len(split) > 3 {
+				svcName := split[len(split)-3]
+				imports = append(imports, svcName)
+			}
+
+		}
+	}
+	return imports, nil
+}
+
+func importHasPrefix(imp *ast.ImportSpec, prefix string) bool {
+	// The import literal contains the " of the string, that why we have to add " to the prefix for
+	// prefix checking
+	return strings.HasPrefix(imp.Path.Value, "\""+prefix)
+}
+
+func importHasSuffix(imp *ast.ImportSpec, suffix string) bool {
+	return strings.HasSuffix(imp.Path.Value, suffix+"\"")
+}
+
+func readModuleName(monoRepoPath string) (string, error) {
+	bytes, err := os.ReadFile(path.Join(monoRepoPath, "go.mod"))
+	if err != nil {
+		return "", err
+	}
+
+	module, _, found := strings.Cut(string(bytes), "\n")
+	if !found {
+		return "", fmt.Errorf("invalid go.mod")
+	}
+
+	return strings.TrimPrefix(module, "module "), nil
 }
